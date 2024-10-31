6399404682494dc86cbe7c564b89c1a7b91ee959
Ruslan Fatkulin <rusya86hmrus@gmail.com>
2024-10-31 08:05:41 +0500

    sdsgdsgd

--- api_market/serializers.py
+++ api_market/serializers.py
@@ -1,15 +1,25 @@
 from rest_framework import serializers
 from market import models
+class SubCategorySerializer(serializers.ModelSerializer):
+    slug = serializers.SlugField()
+    img = serializers.ImageField()
+
+    class Meta:
+        model = models.SubCategory
+        fields = ['slug', 'img']
+
 
 class CategorySerializer(serializers.ModelSerializer):
     title = serializers.CharField()
     slug = serializers.SlugField()
     img = serializers.ImageField()
+    subcategories = SubCategorySerializer(many=True)
     class Meta:
         model = models.Category
         fields = ['title','slug' ,'img', 'subcategories']
 
-class SubCategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.SubCategory
-        fields = ['slug', 'img']
+class ProductSerializer(serializers.ModelSerializer):
+    title = serializers.CharField()
+    slug = serializers.SlugField()
+    price = serializers.IntegerField()
+    subcategories = serializers.
